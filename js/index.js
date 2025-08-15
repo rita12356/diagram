ac46822878af0f9184ed5fd4b75ee5c0d2afeee0
rita12356 <wadeew2016@yandex.ru>
2025-08-15 15:15:23 +0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,76 @@
+let container = document.querySelector(`.items-container`);
+
+ container.innerHTML += `<div
+ class="item" 
+ style=" background-color: #AEB4A9; height: 30px " > 
+1
+</div>`   
+            
+ 
+container.innerHTML += `<div
+class="item" 
+style=" background-color: #E0C1B3; height: 80px " > 
+2
+</div>`   
+      
+container.innerHTML += `<div
+class="item" 
+style=" background-color: #D89A9E; height: 100px " > 
+3
+</div>`   
+      
+container.innerHTML += `<div
+class="item" 
+style=" background-color: #C37D92; height: 50px " > 
+4
+</div>`
+
+
+container.innerHTML += `<div
+class="item" 
+style=" background-color: #846267; height: 60px " > 
+5
+</div>`   
+      
+container.innerHTML += `<div
+class="item" 
+style=" background-color: #C7EDE4; height: 75px " > 
+6
+</div>`   
+      
+container.innerHTML += `<div
+class="item" 
+style=" background-color: #D5FFF3; height: 150px " > 
+7
+</div>`   
+      
+container.innerHTML += `<div
+class="item" 
+style=" background-color: #AF9AB2; height: 200px " > 
+8
+</div>`   
+      
+container.innerHTML += `<div
+class="item" 
+style=" background-color: #820B8A; height: 160px " > 
+9
+</div>`   
+      
+container.innerHTML += `<div
+class="item" 
+style=" background-color: #672A4E; height: 120px " > 
+10
+</div>`   
+      
+container.innerHTML += `<div
+class="item" 
+style=" background-color: #D4D6B9; height: 350px " > 
+11
+</div>`   
+      
+container.innerHTML += `<div
+class="item" 
+style=" background-color: #D1CAA1; height: 300px " > 
+12
+</div>`   
+      
